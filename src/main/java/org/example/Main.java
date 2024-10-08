429e2387aabb66f7cb7d2a874f47cdf0803e8eac
sash000k <aleksander.pr2010@yandex.ru>
2024-10-08 14:07:53 +0300

    Lesson_7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,37 @@
 package org.example;
 
 public class Main {
-    public static void Calculation(String[][] data){
-        int[][] temp = new int[4][4];
-        int lenX = Math.max(temp.length, data.length);
-        int lenY = data[0].length;
-        try{
-            for (int x = 0; x < lenX; x++){
-                lenY = Math.max(temp[x].length, data[x].length);
-            }
-            for (int i = 0; i < lenX; i++) {
-                for (int j = 0; j < lenY; j++){
-                    try{temp[i][j] = Integer.parseInt(data[i][j]);}
-                    catch(NumberFormatException e){
-                        System.out.println("В ячейке ["+i+"]["+j+"] находится символ, который нельзя преобразовать в int");
-                        //e.printStackTrace();
-                    }
+    public static int Calculation(String[][] data) throws MyArrayDataException,MyArraySizeException{
+        int sum=0;
+        for(String[] row: data){
+            if(row.length!=4){throw new MyArraySizeException("некорректный размер по X: "+row.length);}
+            if(data.length!=4){throw new MyArraySizeException("некорректный размер по Y: "+data.length);}
+        }
+        for(int i = 0; i < data.length; i++){
+            for(int j = 0; j < data[i].length; j++){
+                try{
+                    sum += Integer.parseInt(data[i][j]);
+                }
+                catch(NumberFormatException e){
+                    throw new MyArrayDataException("символ \""+data[i][j]+"\" (["+i+"]["+j+"]) " +
+                            "не может быть преобразован в int");
                 }
             }
-            System.out.println("done");
-        }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Неверный размер массива ["+lenX+"]["+lenY+"]");
-            //e.printStackTrace();
         }
+        return sum;
     }
 
     public static void main(String[] args) {
-        String[][] data =  new String[][]{{"123", "222", "333", "452"},{"123", "222", "q", "452"},{"123", "222", "333", "452"},{"123", "222", "333", "452"}};
-        Calculation(data);
+        String[][] data =  new String[][]{
+                {"123", "222", "333", "452"},
+                {"123", "222", "22", "452"},
+                {"123", "222", "Q", "452"},
+                {"123", "222", "333", "452"}
+        };
+        try{
+            System.out.println(Calculation(data));
+        }catch (MyArraySizeException| MyArrayDataException e) {
+            System.out.println("что-то пошло не так: " +e.getMessage());
+        }
     }
 }
