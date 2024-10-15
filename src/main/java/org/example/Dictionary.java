900f8d34222f2747b458eccf1c6fe85b2de24a04
sash000k <aleksander.pr2010@yandex.ru>
2024-10-15 16:01:57 +0300

    Lesson_8

--- src/main/java/org/example/Dictionary.java
+++ src/main/java/org/example/Dictionary.java
@@ -1,16 +1,20 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Dictionary {
-    HashMap<String, String> hm = new HashMap<>();
-    public Dictionary(HashMap<String, String> input){
-        this.hm = input;
-    }
+    HashMap<String, String> dictionary = new HashMap<>();
     public void add(String fio, String number){
-        this.hm.put(fio,number);
+        this.dictionary.put(number,fio);
     }
     public String get(String fio){
-        return this.hm.get(fio);
+        StringBuilder result = new StringBuilder("По фамилии " + fio + " найдено:");
+        for(Map.Entry<String, String> entry : dictionary.entrySet()){
+            if(entry.getValue().equals(fio)){
+                result.append("\n").append(entry.getKey());
+            }
+        }
+        return result.toString();
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,36 @@
 package org.example;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
-    public static String duplicate(ArrayList<String> input){
-        System.out.println("arr "+input);
-        return "return";
+    public static String wordProcessor(ArrayList<String> input){
+        HashMap<String,Integer> hashMap = new HashMap<>();
+        for (String s : input) {
+            hashMap.put(s, hashMap.getOrDefault(s, 0) + 1);
+        }
+        Set<String> unique = hashMap.keySet();
+        StringBuilder duplicates= new StringBuilder();
+        for (Map.Entry<String, Integer> entry : hashMap.entrySet()){
+            duplicates.append(entry.getKey()).append(" - ").append(entry.getValue()).append(" раз\n");
+        }
+        return "Уникальные слова: "+unique+"\n" +
+                "Повторяющиеся слова: " + duplicates;
     }
     public static void main(String[] args) {
         System.out.println("start");
+        System.out.println("--------1----------");
         ArrayList<String> arrayList = new ArrayList<>(Arrays.asList("кот","машина",
                 "динозавр","каша","велосипед","кот","пачка","отпуск","ноутбук","флешка",
                 "динозавр","отдых","ноутбук","кот","бутылка"));
-        System.out.println(duplicate(arrayList));
+        System.out.println(wordProcessor(arrayList));
+        System.out.println("--------2-----------");
+        Dictionary dict = new Dictionary();
+        dict.add("Иванов","+375259379992");
+        dict.add("Петров","+375295553535");
+        dict.add("Сидоров","+375331112233");
+        dict.add("Петров","+375292233445");
+        dict.add("Иванов","+375448877665");
+        dict.add("Петров","+375332245566");
+        System.out.println(dict.get("Иванов"));
     }
 }
